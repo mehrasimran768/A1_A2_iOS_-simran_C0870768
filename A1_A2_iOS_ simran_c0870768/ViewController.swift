266e67e02de4be512809acef7272a49b226a0047
simran mehra <simranmehra.iv@gmail.com>
2023-01-20 17:27:12 -0500

    created button and the privacy request

--- A1_A2_iOS_ simran_c0870768/ViewController.swift
+++ A1_A2_iOS_ simran_c0870768/ViewController.swift
@@ -6,12 +6,24 @@
 //
 
 import UIKit
+import MapKit
+class ViewController: UIViewController , CLLocationManagerDelegate{
 
-class ViewController: UIViewController {
-
+    @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var direction: UIButton!
+    @IBOutlet weak var zoomIn: UIButton!
+    @IBOutlet weak var zoomOut: UIButton!
+    var locationManager: CLLocationManager!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+
+        let latitude: CLLocationDegrees = 43.64
+        let longitude: CLLocationDegrees = -79.38
     }
 
 
